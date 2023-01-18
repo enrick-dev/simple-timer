7a16855e406715aee0d3ad9bfcb16d0ae29e2b25
Enrick Santos de Souza <enricksantos03@gmail.com>
2023-01-18 20:10:32 -0300

    update: functions- btn start & input place number

--- js/progressIndicator.js
+++ js/progressIndicator.js
@@ -2,84 +2,91 @@ const semiCircles = document.querySelectorAll('.semiCircle');
 const miniCircle = document.querySelector('.miniCircleContainer');
 const clockNumbers = document.querySelector('.clock');
 const clockNumbersInput = document.querySelectorAll('.clockInput');
-
-const hr = 0;
-const min = 0;
-const sec = 6;
-
-const hours = hr * 3600000;
-const minutes = min * 60000;
-const seconds = sec * 1000;
-const setTime = hours + minutes + seconds;
-const startTime = Date.now();
-const futureTime = startTime + setTime;
-
-const timerLoop = setInterval(countDownTimer);
-countDownTimer();
-
-function countDownTimer() {
-  const currentTime = Date.now();
-  const remaingTime = futureTime - currentTime;
-  const angle = (remaingTime / setTime) * 360;
+const start = document.getElementById('start')
 
 
-  // barra de progresso
-  if(angle > 180) {
-    semiCircles[2].style.display = 'none';
-    semiCircles[0].style.transform = 'rotate(180deg)';
-    semiCircles[1].style.transform = `rotate(${angle}deg)`;
-    miniCircle.style.transform = `rotate(${angle}deg)`;
-  } else {
-    semiCircles[2].style.display = 'block';
-    semiCircles[0].style.transform = `rotate(${angle}deg)`;
-    semiCircles[1].style.transform = `rotate(${angle}deg)`;
-    miniCircle.style.transform = `rotate(${angle}deg)`;
-  }
+start.addEventListener('click', () => {
+  let hr = document.getElementById('hours');
+  let min = document.getElementById('minutes');
+  let sec = document.getElementById('seconds');
 
 
-  //numeros do timer
-  const hrs = Math.floor((remaingTime / (1000 * 60 * 60)) % 24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-  const mins = Math.floor((remaingTime / (1000 * 60)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-  const secs = Math.ceil((remaingTime / (1000)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+  const hours = hr.value * 3600000;
+  const minutes = min.value * 60000;
+  const seconds = sec.value * 1000;
+  const setTime = hours + minutes + seconds;
+  const startTime = Date.now();
+  const futureTime = startTime + setTime;
 
 
-  clockNumbers.innerHTML = `
-    <input type="number" class="clockInput" placeholder="${hrs}">
-    <input type="number" class="clockInput" placeholder="${mins}">
-    <input type="number" class="clockInput" placeholder="${secs}">
 
-    <style>
-      input::-webkit-outer-spin-button,
-      input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-      }
-      .clockInput {
-        cursor: default;
-      }
-    </style>
-
-  `;
-
-  // if(remaingTime <= 5000) {
-  //   semiCircles[0].style.backgroundColor = '#9229d4';
-  //   semiCircles[1].style.backgroundColor = '#9229d4';
-  //   clockNumbers.style.color = '#9229d4';
-  // }
-
-  if(remaingTime <= 0) {
-    clearInterval(timerLoop);
-    semiCircles[0].style.display = 'none';
-    semiCircles[1].style.display = 'none';
-    semiCircles[2].style.display = 'none';
-
+  const timerLoop = setInterval(countDownTimer);
+  function countDownTimer() {
+    const currentTime = Date.now();
+    const remaingTime = futureTime - currentTime;
+    const angle = (remaingTime / setTime) * 360;
+  
+  
+    // barra de progresso
+    if(angle > 180) {
+      semiCircles[2].style.display = 'none';
+      semiCircles[0].style.transform = 'rotate(180deg)';
+      semiCircles[1].style.transform = `rotate(${angle}deg)`;
+      miniCircle.style.transform = `rotate(${angle}deg)`;
+    } else {
+      semiCircles[2].style.display = 'block';
+      semiCircles[0].style.transform = `rotate(${angle}deg)`;
+      semiCircles[1].style.transform = `rotate(${angle}deg)`;
+      miniCircle.style.transform = `rotate(${angle}deg)`;
+    }
+  
+  
+    //numeros do timer
+    const hrs = Math.floor((remaingTime / (1000 * 60 * 60)) % 24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    const mins = Math.floor((remaingTime / (1000 * 60)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    const secs = Math.ceil((remaingTime / (1000)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+  
+  
     clockNumbers.innerHTML = `
-      <input type="number" class="clockInput" placeholder="00">
-      <input type="number" class="clockInput" placeholder="00">
-      <input type="number" class="clockInput" placeholder="00">
+      <input type="number" class="clockInput" placeholder="${hrs}" max="99" min="0">
+      <input type="number" class="clockInput" placeholder="${mins}" max="99" min="0">
+      <input type="number" class="clockInput" placeholder="${secs}" max="99" min="0">
+  
+      <style>
+        // input::-webkit-outer-spin-button,
+        // input::-webkit-inner-spin-button {
+        //   -webkit-appearance: none;
+        //   margin: 0;
+        // }
+        .clockInput {
+          cursor: default;
+        }
+      </style>
+  
     `;
-    clockNumbers.style.color = '#ceade3';
+  
+    // if(remaingTime <= 5000) {
+    //   semiCircles[0].style.backgroundColor = '#9229d4';
+    //   semiCircles[1].style.backgroundColor = '#9229d4';
+    //   clockNumbers.style.color = '#9229d4';
+    // }
+  
+    if(remaingTime <= 0) {
+      clearInterval(timerLoop);
+      semiCircles[2].style.display = 'block';
+      semiCircles[0].style.transform = 'rotate(0deg)';
+      semiCircles[1].style.transform = `rotate(0deg)`;
+      miniCircle.style.transform = `rotate(0deg)`;
+  
+      clockNumbers.innerHTML = `
+        <input type="number" class="clockInput" id="hours" placeholder="00" max="99" min="0">
+        <input type="number" class="clockInput" id="minutes" placeholder="00" max="99" min="0">
+        <input type="number" class="clockInput" id="seconds" placeholder="00" max="99" min="0">
+      `;
+      clockNumbers.style.color = '#ceade3';
+    }
   }
-}
+})
+
 
 
